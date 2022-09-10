c1e2a5bf838a0ce05a8b99d3c4f6777c5c0fc441
Peter Stovka <stovka.peter@gmail.com>
2022-09-10 10:19:47 +0200

    wip

---
+++ src/Providers/ElementsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Insight\Elements\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class ElementsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // TEST
+    }
+
+    public function boot()
+    {
+
+    }
+}
