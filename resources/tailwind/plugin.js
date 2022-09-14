18aa881e910e5248a98701cdb1b71954a0e08c98
Peter <stovka.peter@gmail.com>
2022-09-14 06:46:53 +0000

    add breadcrumbs

--- resources/tailwind/plugin.js
+++ resources/tailwind/plugin.js
@@ -4,11 +4,17 @@ const colors = require('tailwindcss/colors')
 const addButtons = require('./buttons')
 const addForms = require('./forms')
 const addCard = require('./card')
+const addBadges = require('./badges')
+const addOutlineMenu = require('./outline-menu')
+const addBreadcrumbs = require('./breadcrumbs')
 
 module.exports = plugin(function(options) {
   addButtons(options)
   addCard(options)
   addForms(options)
+  addBadges(options)
+  addOutlineMenu(options)
+  addBreadcrumbs(options)
 }, {
   theme: {
     extend: {
